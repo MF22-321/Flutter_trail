a007367c1e6f41d2baceb8b9f2659e4dfec912fc
Muhamad Febrian <137358003+MF22-321@users.noreply.github.com>
2024-12-20 15:15:01 +0700

    Create test.py

---
+++ test.py
@@ -0,0 +1,7 @@
+echo "# Voice_command" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/MF22-321/Voice_command.git
+git push -u origin main
